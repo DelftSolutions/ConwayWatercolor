a34ccda2ce5b2e6fc9ae3ac52fe5aa23468d3c3e
Max Maton <info@maxmaton.nl>
2024-11-04 02:16:53 +0100

    Initial Commit

---
+++ ConwayWatercolor/ConwayWatercolorView.h
@@ -0,0 +1,12 @@
+//
+//  ConwayWatercolorView.h
+//  ConwayWatercolor
+//
+//  Created by Max Maton on 04/11/2024.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface ConwayWatercolorView : ScreenSaverView
+
+@end
---
+++ ConwayWatercolor/ConwayWatercolorView.m
@@ -0,0 +1,51 @@
+//
+//  ConwayWatercolorView.m
+//  ConwayWatercolor
+//
+//  Created by Max Maton on 04/11/2024.
+//
+
+#import "ConwayWatercolorView.h"
+
+@implementation ConwayWatercolorView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
